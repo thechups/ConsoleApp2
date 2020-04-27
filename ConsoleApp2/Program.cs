f953387429f95b7a6708e95613b575bf0cf40c25
thechups <sergei.ustinkin@gmail.com>
2020-04-27 10:32:34 +0300

    Initial commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "somefile.name";
+            var fileNameWithSuffix = fileName.AddSuffix("suffix");
+        }
+    }
+}
---
+++ ConsoleApp2/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp2
+{
+    using System.IO;
+
+    public static class StringExtensions
+    {
+        public static string AddSuffix(this string fileName, string suffix)
+        {
+            var directoryName = Path.GetDirectoryName(fileName);
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            var extension = Path.GetExtension(fileName);
+
+            return Path.Combine(
+                directoryName ?? string.Empty,
+                string.Concat(fileNameWithoutExtension, suffix, extension));
+        }
+    }
+}
